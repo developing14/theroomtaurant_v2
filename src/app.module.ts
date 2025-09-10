049c061f891e6a741103fb63b7b0ab4d213d13d2
quang <quangb2016996@student.ctu.edu.vn>
2025-09-10 12:29:59 +0700

    Config using EV

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 import { AccountModule } from './account/account.module'
 import { AuthModule } from './auth/auth.module';
@@ -13,20 +13,27 @@ import { AttendanceModule } from './attendance/attendance.module';
 import { PositionModule } from './position/position.module';
 import { CandidateModule } from './candidate/candidate.module';
 
+import databaseConfig from './config/database.config'
+
 @Module({
   imports: [
-        TypeOrmModule.forRoot({
-          type: 'mongodb',
-          host: 'localhost',
-          port: 27017,
-          username: '',
-          password: '',
-          database: 'TheRoomtaurantDB',
-          autoLoadEntities: true
-        }),
-        ConfigModule.forRoot({
-            isGlobal: true
-        }),
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+        isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mongodb',
+        host: config.get<string>('database.host'),
+        port: config.get<number>('database.port'),
+        username: config.get<string>('database.username'),
+        password: config.get<string>('database.password'),
+        database: config.get<string>('database.database'),
+        autoLoadEntities: true
+      }),
+    }),
     AccountModule, AuthModule, EmployeeModule, DepartmentModule, AttendanceModule, PositionModule, CandidateModule],
   controllers: [AppController],
   providers: [AppService],
